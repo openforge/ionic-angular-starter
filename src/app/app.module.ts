d0e7cb5d4e17220494570899bc0fab5ad615c4cf
Fdom92 <fer.olmo92@gmail.com>
2019-12-20 12:40:22 +0100

    feat(chore): add angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -6,9 +7,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { NgxsModule } from '@ngxs/store';
 import { AppState } from 'src/app/state/app.state';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +18,8 @@ import { environment } from 'src/environments/environment';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    NgxsModule.forRoot([AppState], { developmentMode: !environment.production })
+    NgxsModule.forRoot([AppState], { developmentMode: !environment.production }),
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [
     StatusBar,
